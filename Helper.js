85dae1a679d7ea3355848369c8efe6ff97719eac
Lergin <lergin@gmx.de>
2016-11-01 20:41:54 +0100

    and once more more jsdocs

--- Helper.js
+++ Helper.js
@@ -1,7 +1,13 @@
+/**
+ * a Helper Class
+ */
 class Helper {
-    /*
+    /**
      * converts an array of objects to an Map where the key of each object is equal to the idField of the corresponding
      * object in the array
+     * @param {Array.<Object>} arr that should be converted
+     * @param {string} idField the field of the object that should be used as key in the created map
+     * @return {Map.<Object>} the map created from the data
      */
     static convertToMap( arr, idField ) {
         let obj = new Map();
