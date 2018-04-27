2af1194d42ac9b17f4e8fc92ee6f291d8ba24513
Lergin <lergin@gmx.de>
2018-04-27 10:32:58 +0200

    removed individual player statistics as this is almost not used by anyone has a lot of bugs and might not be allowed throug GDPR

--- backend_scripts/lib/main.ts
+++ backend_scripts/lib/main.ts
@@ -1,14 +1,9 @@
 import {BotFramework} from 'lergins-bot-framework';
 import * as path from "path";
-import { MainMapPlotsUpdater } from './updater/MainMapPlotsUpdater';
 import { MainMapPlotStatsUpdater } from './updater/MainMapPlotStatsUpdater';
 import { RulesUpdater } from './updater/RulesUpdater';
-import { PlayerListUpdater } from './updater/PlayerListUpdater';
 import { Push } from './notifications/Push';
-import { PlotsUpdater } from './updater/PlotsUpdater';
-import { VotesUpdater } from './updater/VotesUpdater';
 import { WorldStatsUpdater } from './updater/WorldStatsUpdater';
-import { ZonesUpdater } from './updater/ZonesUpdater';
 import { FreeZonesUpdater } from './updater/FreeZonesUpdater';
 
 export const bot = new BotFramework.Builder()
@@ -17,14 +12,9 @@ export const bot = new BotFramework.Builder()
   .forceFirebaseInit()
   .build();
 
-bot.addUpdater(new MainMapPlotsUpdater());
 bot.addUpdater(new MainMapPlotStatsUpdater());
-bot.addUpdater(new PlayerListUpdater());
-bot.addUpdater(new PlotsUpdater());
 bot.addUpdater(new RulesUpdater());
-bot.addUpdater(new VotesUpdater());
 bot.addUpdater(new WorldStatsUpdater());
-bot.addUpdater(new ZonesUpdater());
 bot.addUpdater(new FreeZonesUpdater());
 
 bot.start();
